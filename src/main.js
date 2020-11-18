2873f82bc1b57b805671078ab64e2c97771b8051
Cipto Tri Utomo <ciptredevie@gmail.com>
2020-11-18 19:09:29 +0700

    feat(vss): init vss library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueScreenSize from 'vue-screen-size'
 import '@/services/filter'
 import '@/assets/main.scss'
 
+Vue.use(VueScreenSize)
 Vue.config.productionTip = false
 
 new Vue({
