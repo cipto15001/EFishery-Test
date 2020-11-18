9a2ce7acb6f383743370b0d2f881163fcb9f8a44
Cipto Tri Utomo <ciptredevie@gmail.com>
2020-11-18 19:49:58 +0700

    feat(v-money) : global register v-money

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueScreenSize from 'vue-screen-size'
+import money from 'v-money'
 import '@/services/filter'
 import '@/assets/main.scss'
 
 Vue.use(VueScreenSize)
+Vue.use(money, {
+  decimal: ',',
+  thousands: '.',
+  prefix: 'Rp ',
+  precision: 0,
+  masked: false,
+})
 Vue.config.productionTip = false
 
 new Vue({
